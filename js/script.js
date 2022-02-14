41b0e4b0f8991c985c1e7a4cb3d12a6a8b55a9cf
Borovskyi93 <85964928+Borovskyi93@users.noreply.github.com>
2022-02-14 21:32:15 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+//////////////////////
+let name = 'John';
+let admin = name; 
+alert(admin);
+//////////////////////
+let planetEarth; 
+let userName;
+//////////////////////
+let exampleString = '10' + 10; 
+console.log(exampleString);
